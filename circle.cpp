46cfd411c2dcc244474ffdb0b103e045db743787
arpitarangolkar-cyber <arpitarangolkar@gmail.com>
2026-07-21 09:19:45 +0530

    Add files via upload

---
+++ circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int r,area,circum;
+  float pi=3.14;
+  cin>>r;
+  area=pi*r*r;
+  circum=2*pi*r;
+  cout<<"area:"<<area<<endl;
+  cout<<"circum:"<<circum<<endl;
+}
---
+++ multipleoperations.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,sum,sub,multi,div,mod;
+  cin>>a>>b;
+  sum=a+b;
+  sub=a-b;
+  multi=a*b;
+  div=a/b;
+  mod=a%b;
+  cout<<"***The operations are:"<<endl;
+  cout<<"addition:"<<sum<<endl;
+  cout<<"subtraction:"<<sub<<endl;
+  cout<<"multiplication:"<<multi<<endl;
+  cout<<"division:"<<div<<endl;
+  cout<<"modulas:"<<mod<<endl;
+}
